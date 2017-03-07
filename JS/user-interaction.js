1aa62fec6a4c56f31620bd8d92902611318e74f8
Skitak <bas2205@live.fr>
2017-03-07 14:34:59 +0100

    utilisateur cliquant ajoute

---
+++ JS/user-interaction.js
@@ -0,0 +1,39 @@
+var i = 1; 
+
+$(function () {
+     $('.button').click(function () {
+         --i;
+         if (i >= 0)
+             $('.compteur').text(i);
+         if (i == 0){
+             $("p:nth-child(3)").text("tadaaaa");
+             $("p:nth-child(5)").css('display', 'none');
+             $("#surprise").css('display', 'block');
+             $("button").css('display', 'none');
+         }
+     });
+ });
+
+ /*$(function () {
+       $('#majPremier').click(function () {
+           $('#premier').load('bonjour.php');
+       });
+
+       $('#action').click(function () {//Comme le prof le voulait
+           $.ajax({
+               type: 'GET',
+               url: 'bonjour.php',
+               timeout: 3000,
+               success: function (data) {
+                   alert(data);
+               },
+               error: function () {
+                   alert('La requête n\'a pas abouti');
+               }
+           });
+       });
+   });*/
+
+ //Leaflet et mapquest dans l'ordre.
+ //Leaflet est une api de haut niveau pour afficher des maps en json.
+ //Mapquest est une api au dessus de leaflet, et permet de créer des parcours.
