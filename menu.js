296f62913f3ca64806a598bcecf23e32904fd099
Skitak <bas2205@live.fr>
2017-03-07 12:47:21 +0100

    menu nav added

---
+++ menu.js
@@ -0,0 +1,28 @@
+ $(function () {
+      $('nav li:nth-child(1)').click(function () {
+          $('section').load('blocs.html section', function(){alert("");});
+      });
+
+      $('nav li:nth-child(2)').click(function () {
+          $('section').load('echange-client-serveur.html');
+      });
+     $('nav li:nth-child(3)').click(function () {
+          $('section').load('json.html');
+      });
+     
+     $('nav li:nth-child(4)').click(function () {
+          $('section:first-child').load('echange-php.html');
+      });
+     
+     $('nav li:nth-child(5)').click(function () {
+          $('section').load('carte.html');
+      });
+     
+     $('nav li:nth-child(6)').click(function () {
+          $('section').load('superposition.html');
+      });
+     
+     $('nav li:nth-child(7)').click(function () {
+          $('section').load('user-interaction.html');
+      });
+  });
