f8c66ba39b181fa18856a96af1e97b247f1b4488
Skitak <bas2205@live.fr>
2017-03-08 16:48:23 +0100

    info bdd added

--- connexion.php
+++ connexion.php
@@ -1,1 +1,10 @@
-Ok
+<?php
+echo 'Ok';
+// Recherche dans la BDD.
+//Si l'utilisateur existe, alors on vérifie son mdp.
+//Sinon, on le crée.
+// Adresse de la bdd : 	db672391189.db.1and1.com
+// Nom d'utilisateur :  dbo672391189
+// Nom de la bdd     :  db672391189
+// mot de passe      :  123456789
+?>
