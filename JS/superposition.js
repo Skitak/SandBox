23c248378a8c2bbd47116fb1c9e806032fc4df65
bas2205@live.fr <bas2205@live.fr>
2017-03-08 21:00:56 +0100

    blocs modified

--- JS/superposition.js
+++ JS/superposition.js
@@ -2,8 +2,18 @@ var blocs;
 
 $(function () {
     blocs = $(".bloc").draggable({
-        start : function( event, ui ) {
+        start: function (event, ui) {
+            blocs.css("z-index", "0");
+            ui.helper.css("z-index", "1");
+        }
+    });
+    blocs.click(function () {
         blocs.css("z-index", "0");
-        ui.helper.css("z-index", "1");
-    }});
- });
+        $(this).css("z-index", "1");
+        var R = Math.floor((Math.random() * 256));
+        var G = Math.floor((Math.random() * 256));
+        var B = Math.floor((Math.random() * 256));
+        $(this).css("background-color", "rgb(" + R + "," + G + "," + B + ")" );
+        $(this).css("box-shadow", "0px 0px 50px rgb(" + R + "," + G + "," + B + ")" );
+    })
+});
